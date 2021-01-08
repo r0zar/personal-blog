fcaf29e07018234e730825f878f363b0150eabaf
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-01-08 17:20:21 -0600

    Update font.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,13 +14,13 @@
 </script>
   
 <style>
-	h1, figure, p {
+	h1, figure {
 		text-align: center;
 		margin: 0 auto;
 	}
 
 	h1 {
-		font-size: 2.8em;
+		font-size: 2em;
 		text-transform: uppercase;
 		font-weight: 700;
 		margin: 0 0 0.5em 0;
@@ -28,16 +28,6 @@
 
 	figure {
 		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
 	}
 
 	@media (min-width: 480px) {
