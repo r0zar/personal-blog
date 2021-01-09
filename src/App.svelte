b7702768ef44922509f491484ec6fbcf5d24ac8d
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-01-09 06:24:44 -0600

    Clean up

--- src/App.svelte
+++ src/App.svelte
@@ -65,9 +65,9 @@
         >Home</a
       >
       <a
-        href="#"
+        href="https://blog.rossragsdale.com"
         class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2"
-        >About</a
+        >Blog</a
       >
       <a
         href="#"
