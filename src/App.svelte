fa5c8719c83fb145c30eca780799154f982124ae
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-01-08 20:32:33 -0600

    Add styles and nice things.

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,21 @@
 main {
   text-align: center;
   padding: 1em;
-  max-width: 240px;
+  max-width: 320px;
   margin: 0 auto;
   flex-grow: 1;
 }
 
 footer > svg {
   margin-bottom: -3px;
+  bottom: 0;
+  z-index: -1;
 }
 
 h1 {
   font-size: 4em;
   font-weight: 100;
+  color: #0099ff;
 }
 
 @media (min-width: 640px) {
@@ -24,9 +27,30 @@ h1 {
 </style>
 
 <script>
+import Tailwindcss from "./Tailwind.svelte";
 export let name;
 import anime from "animejs/lib/anime.es.js";
 import { onMount } from "svelte";
+import GhostContentAPI from "@tryghost/content-api";
+const api = new GhostContentAPI({
+  url: "http://localhost:2368",
+  key: "a57254b24d92ef3ef970931803",
+  version: "v3",
+});
+let postList = [];
+// fetch 5 posts, including related tags and authors
+api.posts
+  .browse({ limit: 5, include: "tags,authors" })
+  .then((posts) => {
+    posts.forEach((post) => {
+      console.log(post.title);
+    });
+    postList = posts;
+    console.log(postList);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 onMount(() => {
   const svgPath = document.querySelectorAll(".path");
   const svgText = anime({
@@ -43,9 +67,40 @@ onMount(() => {
 });
 </script>
 
+<Tailwindcss />
+
 <main>
   <h1>hi, i'm ross</h1>
   <p>Welcome to my site.</p>
+  <section class="py-12">
+    <div class="container mx-auto lg:w-1/2">
+      <div class="flex flex-col">
+        {#each postList as post}
+          <div class="w-full md:px-4 lg:px-6 py-5">
+            <div class="bg-white hover:shadow-md shadow rounded-lg">
+              <!-- <div class="rounded-lg rounded-b-none">
+                <img
+                  src="{post.feature_image}"
+                  alt="{post.title}"
+                  class="w-full h-48 object-cover rounded-lg rounded-b-none " />
+              </div> -->
+              <div class="px-4 py-4 md:px-10">
+                <h2 class="font-bold text-md">{post.title}</h2>
+                <p class="py-4">
+                  {post.excerpt}
+                </p>
+                <div class="flex flex-wrap pt-8">
+                  <div class="w-full text-sm font-medium">
+                    {new Date(post.created_at).toLocaleDateString()}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        {/each}
+      </div>
+    </div>
+  </section>
 </main>
 <footer>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
