8613f530db511d8357780ded2e77e92f3334b846
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-01-09 06:22:00 -0600

    Clean up

--- src/App.svelte
+++ src/App.svelte
@@ -36,7 +36,7 @@
 
 <Tailwindcss />
 
-<nav class="bg-white shadow-sm">
+<nav>
   <div class="md:flex items-center justify-between py-3 px-8 md:px-12">
     <div class="flex justify-between items-center">
       <div class="text-2xl font-bold text-gray-800 md:text-3xl">
