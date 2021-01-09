42f56ef1c05cea1d75bc389917a4c19dd580caba
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-01-09 06:25:17 -0600

    Clean up

--- src/App.svelte
+++ src/App.svelte
@@ -70,9 +70,9 @@
         >Blog</a
       >
       <a
-        href="#"
+        href="https://pointblankdev.com"
         class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2"
-        >Contact</a
+        >Point Blank Dev</a
       >
     </div>
   </div>
