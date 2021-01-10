4a8630ec282b2358cc8a5963fe0996e9615625b0
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-01-10 06:33:32 -0600

    Fix social icons.

--- src/App.svelte
+++ src/App.svelte
@@ -137,8 +137,15 @@
   </section>
 </main>
 <footer>
+  <svg class="-mb-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path
+      fill="#0099ff"
+      fill-opacity="1"
+      d="M0,160L48,181.3C96,203,192,245,288,229.3C384,213,480,139,576,106.7C672,75,768,85,864,106.7C960,128,1056,160,1152,192C1248,224,1344,256,1392,272L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+    />
+  </svg>
   <div
-    class="invisible sm:visible absolute sm:bottom-6 mx-auto left-0 right-0 text-center cursor-default"
+    class="invisible sm:visible mb-3 mx-auto left-0 right-0 text-center cursor-default"
   >
     <a href="https://discord.gg/StNEj3trxt"
       ><i
@@ -164,13 +171,6 @@
       /></a
     >
   </div>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-    <path
-      fill="#0099ff"
-      fill-opacity="1"
-      d="M0,160L48,181.3C96,203,192,245,288,229.3C384,213,480,139,576,106.7C672,75,768,85,864,106.7C960,128,1056,160,1152,192C1248,224,1344,256,1392,272L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-    />
-  </svg>
 </footer>
 
 <style>
